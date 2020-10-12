a4dcb44eb5eabbe9e6cfdbccca392b49d5877928
lukasz-madej <lukasz.madej@kreuzwerker.pl>
2020-10-12 20:55:24 +0200

    Add error handling

--- src/app/core/api/services/api.service.ts
+++ src/app/core/api/services/api.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { SnackBarService } from '../../../shared/services/snack-bar/snack-bar.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,21 +12,35 @@ export class ApiService {
 
   private readonly _apiUrl: string;
 
-  constructor(private _http: HttpClient) {
+  constructor(private _http: HttpClient, private _snackBarService: SnackBarService) {
     this._apiUrl = environment.apiUrl;
   }
 
   get = (endpoint: string): Observable<any> => {
-    return this._http.get(this._getUrl(endpoint));
+    return this._http.get(this._getUrl(endpoint))
+      .pipe(
+        catchError(this._showErrorSnackBar)
+      );
   }
 
   post = (endpoint: string, payload: any): Observable<any> => {
-    return this._http.post(this._getUrl(endpoint), payload);
+    return this._http.post(this._getUrl(endpoint), payload)
+      .pipe(
+        catchError(this._showErrorSnackBar)
+      );
   }
 
   delete = (endpoint: string): Observable<any> => {
-    return this._http.delete(this._getUrl(endpoint));
+    return this._http.delete(this._getUrl(endpoint))
+      .pipe(
+        catchError(this._showErrorSnackBar)
+      );
   }
 
   private _getUrl = (endpoint: string): string => this._apiUrl + endpoint;
+
+  private _showErrorSnackBar = (error: HttpErrorResponse): Observable<any> => {
+    this._snackBarService.open('Coś poszło nie tak. Sróbuj ponownie później.');
+    return throwError(error);
+  }
 }
