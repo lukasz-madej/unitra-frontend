6587a39773c4a052bd3cd19605df5b427fb442c6
lukasz-madej <lukasz.madej@kreuzwerker.pl>
2020-10-12 21:32:20 +0200

    Add category add dialog

---
+++ src/app/modules/categories/components/add-category/add-category.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { CategoryService } from '../../../../shared/services/category/category.service';
+
+@Component({
+  selector: 'app-add-category',
+  templateUrl: './add-category.component.html',
+  styleUrls: ['./add-category.component.scss']
+})
+export class AddCategoryComponent implements OnInit {
+
+  categoryForm: FormGroup;
+
+  get name(): AbstractControl {
+    return this.categoryForm.get('name');
+  }
+  get description(): AbstractControl {
+    return this.categoryForm.get('description');
+  }
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _mdDialogRef: MatDialogRef<AddCategoryComponent>,
+    private _categoryService: CategoryService
+  ) { }
+
+  ngOnInit(): void {
+    this.categoryForm = this._formBuilder.group({
+      name: ['', [Validators.required]],
+      description:  [''],
+      active: [true]
+    });
+  }
+
+  onAdd = (): void => {
+    if (this.categoryForm.valid) {
+      this._categoryService.add(this.categoryForm.value)
+        .subscribe((): void => {
+          this._close();
+        });
+    }
+  }
+
+  onCancel = (): void => {
+    this._close();
+  }
+
+  private _close = (): void => {
+    this._mdDialogRef.close();
+  }
+}
